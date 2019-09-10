ede956441f0345e32f9fe5c50aee443d3d3e9639
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-10 23:33:09 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,11 +2,12 @@ class Book
 
   attr_accessor :title
   attr_accessor :author
-  attr_accessor :page_count(= 0)
+  attr_accessor :page_count
   attr_accessor :genre
 
   def initialize(title)
     @title = title
+    @page_count = 0
   end
 
   def turn_page
